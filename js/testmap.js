8034c75872bbfd5c7396e908e5bdf4740c049997
Sam Chaffee <sam.chaffee@gmail.com>
2014-01-06 08:51:34 -0700

    Added Leaflet support to default layout; test Leaflet post

---
+++ js/testmap.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                                        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
